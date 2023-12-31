2a7284b327b89e8eda51050a2765d944dc38d84b
unimat <unimat@unimat.hu>
2023-12-31 09:41:42 +0100

    Create the Header Section

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,7 @@
 import { Inter } from 'next/font/google'
+import Header from '@/components/Header';
+import Navbar from "@/components/Navbar";
+import SearchBox from "@/components/SearchBox";
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -11,7 +14,17 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {/* Header */}
+        <Header />
+
+        {/* Navbar */}
+        <Navbar />
+
+        {/* SearchBox */}
+        <SearchBox />
+        {children}
+      </body>
     </html>
   )
 }
