648144c492374de9d366ba4f03b012476780c3dc
unimat <unimat@unimat.hu>
2024-01-01 10:36:06 +0100

    Fetch data from TMDB API in the Home Page.

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,45 @@
-import Image from 'next/image'
+import Results from "@/components/Results";
 
-export default function Home() {
+const API_KEY = process.env.API_KEY
+
+export default async function Home({searchParams}) {
+  const genre = searchParams.genre || "fetchTrending";
+
+  //**Authentication with Bearer token**//
+  // const url = `https://api.themoviedb.org/3/${
+  //   genre === "fetchTopRated" ? "movie/top_rated" : "trending/all/week"
+  // }?language=en-US&page=1&sort_by=created_at.asc`
+
+  // const options = {
+  //   method: 'GET',
+  //   headers: {
+  //     accept: 'application/json',
+  //     Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4YmFkNzBkOGJiYjdjODQyNDQzMjE0MDZmYjczNDg2ZCIsInN1YiI6IjY1OTE2NDFkNmFhOGUwNjI3N2VhY2Q5NCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.2ZCphL-zsPsfaxS6uFXwEP-J46DnVuy7SO6_NEgCxS4'
+  //   }
+  // };
+
+  // const res = await fetch( url, options)
+
+  //**Authentication with API Key**//
+  const url = `https://api.themoviedb.org/3/${
+    genre === "fetchTopRated" ? "movie/top_rated" : "trending/all/week"
+  }?api_key=${API_KEY}&language=en-US&page=1`
+  
+  const res = await fetch( 
+    url, 
+    { next: { revalidate: 10000 } }
+  )
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch data"); // this will be caught by the error page and passed to the page as props
+  }
+
+  const data = await res.json();
+  const results = data.results;
+
+  // console.log('Result: ', results)
+  
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Home Page</h1>
-    </main>
+    <Results results={results} />
   )
 }
---
+++ components/Results.jsx
@@ -0,0 +1,11 @@
+// import Card from "./Card";
+
+export default function Results({ results }) {
+  return (
+    <div>
+      {results.map((result) => (
+        <div key={result.id}>{result.original_title}</div>
+      ))}
+    </div>
+  );
+}
